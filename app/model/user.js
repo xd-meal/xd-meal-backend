dc08c129ffc67f9038a05e28c954fc4f74017227
Tinko <TinkoLiu@users.noreply.github.com>
2019-12-25 10:01:55 +0800

    feat: use sparse index on email field

--- app/model/user.js
+++ app/model/user.js
@@ -23,10 +23,7 @@ module.exports = app => {
       type: String,
       unique: true,
       trim: true,
-      index: {
-        unique: true,
-        partialFilterExpression: { email: { $type: 'string' } },
-      },
+      sparse: true,
     },
     avatar: {
       type: String,
