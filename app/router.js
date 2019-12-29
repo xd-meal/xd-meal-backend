88d13378c3f9010432ad6bed71357f7920312df4
Tinko <TinkoLiu@users.noreply.github.com>
2019-12-29 11:05:39 +0800

    feat(middleware): add isAdmin middleware

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  const isAdmin = app.middleware.isAdmin;
   router.get('/', controller.home.index);
   router.get('/api/v1', controller.home.index);
 
@@ -18,15 +19,18 @@ module.exports = app => {
   // my dish
   router.get('/api/v1/myDish', controller.dish.pickingMyDish);
 
+  // get all orderable dining
+  // router.get('api/v1/dining/list');
+
   // admin
   router.post('/api/v1/admin/user/list', controller.admin.userList);
-  router.get('/api/v1/admin/dish/list', controller.admin.dishList);
-  router.post('/api/v1/admin/dish', controller.admin.newDish);
-  router.get('/api/v1/admin/dining/:startTime/:endTime', controller.admin.diningByTime);
-  router.post('/api/v1/admin/dining', controller.admin.newDining);
-  router.put('/api/v1/admin/dining/:id', controller.admin.updateDining);
-  router.delete('/api/v1/admin/dining/:id', controller.admin.deleteDiningById);
-  router.get('/api/v1/admin/users', controller.admin.users);
-  router.get('/api/v1/admin/order/:startTime/:endTime', controller.admin.orderByUserIdAndTime);
-  router.put('/api/v1/admin/dish/:id', controller.admin.updateDish);
+  router.get('/api/v1/admin/dish/list', isAdmin, controller.admin.dishList);
+  router.post('/api/v1/admin/dish', isAdmin, controller.admin.newDish);
+  router.get('/api/v1/admin/dining/:startTime/:endTime', isAdmin, controller.admin.diningByTime);
+  router.post('/api/v1/admin/dining', isAdmin, controller.admin.newDining);
+  router.put('/api/v1/admin/dining/:id', isAdmin, controller.admin.updateDining);
+  router.delete('/api/v1/admin/dining/:id', isAdmin, controller.admin.deleteDiningById);
+  router.get('/api/v1/admin/users', isAdmin, controller.admin.users);
+  router.get('/api/v1/admin/order/:startTime/:endTime', isAdmin, controller.admin.orderByUserIdAndTime);
+  router.put('/api/v1/admin/dish/:id', isAdmin, controller.admin.updateDish);
 };
