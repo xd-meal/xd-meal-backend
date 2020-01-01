b6bff72f3942c84e60f3ad5d75c1ad0b6ae956e0
Tinko <TinkoLiu@users.noreply.github.com>
2020-01-01 13:39:13 +0800

    feat(all): add pos machine api

---
+++ app/controller/pos.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+const HttpError = require('../helper/error');
+
+class PosController extends Controller {
+  async performPick() {
+    const ctx = this.ctx;
+    const order = await ctx.service.orderToken.get(ctx.params.token);
+    if (!(order.userId && order.diningId)) {
+      throw new HttpError({
+        code: 403,
+        msg: 'Token 无效',
+      });
+    }
+    const dining = await ctx.service.dining.getDiningByID(order.diningId);
+    if (dining.stat_type !== 0 && !order.orderId) {
+      throw new HttpError({
+        code: 403,
+        msg: 'Token 无效',
+      });
+    }
+    if (dining.stat_type === 0) {
+      ctx.service.order.addOrder({
+        userId: order.userId,
+        diningId: order.diningId,
+        menuId: 0,
+        picked: true,
+      });
+    } else {
+      if (!order.orderId) {
+        throw new HttpError({
+          code: 403,
+          msg: 'Token 无效',
+        });
+      }
+      if (!await ctx.service.order.findByID(order.orderId)) {
+        throw new HttpError({
+          code: 403,
+          msg: 'Token 无效',
+        });
+      }
+      await ctx.service.order.setPicked(order.orderId);
+      ctx.body = {
+        code: 0,
+        msg: '已取餐',
+        data: {
+          dining,
+          order,
+        },
+      };
+    }
+  }
+}
+module.exports = PosController;
