442da9d931ec1af739899685d4e886da581a578a
Tinko <TinkoLiu@users.noreply.github.com>
2020-01-06 14:17:37 +0800

    fix: returning wrong order object on pos api

--- app/controller/pos.js
+++ app/controller/pos.js
@@ -6,42 +6,43 @@ const HttpError = require('../helper/error');
 class PosController extends Controller {
   async performPick() {
     const ctx = this.ctx;
-    const order = await ctx.service.orderToken.get(ctx.params.token);
-    if (!(order && order.userId && order.diningId)) {
+    const orderToken = await ctx.service.orderToken.get(ctx.params.token);
+    if (!(orderToken && orderToken.userId && orderToken.diningId)) {
       throw new HttpError({
         code: 403,
         msg: 'Token 无效',
       });
     }
-    const dining = await ctx.service.dining.getDiningByID(order.diningId);
-    if (dining.stat_type !== 0 && !order.orderId) {
+    const dining = await ctx.service.dining.getDiningByID(orderToken.diningId);
+    if (dining.stat_type !== 0 && !orderToken.orderId) {
       throw new HttpError({
         code: 403,
         msg: 'Token 无效',
       });
     }
     if (dining.stat_type === 0) {
-      ctx.service.order.addOrder({
-        userId: order.userId,
-        diningId: order.diningId,
+      ctx.service.orderToken.addOrder({
+        userId: orderToken.userId,
+        diningId: orderToken.diningId,
         menuId: 0,
         picked: true,
       });
     } else {
-      if (!order.orderId) {
+      if (!orderToken.orderId) {
         throw new HttpError({
           code: 403,
           msg: 'Token 无效',
         });
       }
-      if (!await ctx.service.order.findByID(order.orderId)) {
+      if (!await ctx.service.orderToken.findByID(orderToken.orderId)) {
         throw new HttpError({
           code: 403,
           msg: 'Token 无效',
         });
       }
-      const user = await ctx.service.users.getUserProfile(order.userId);
-      await ctx.service.order.setPicked(order.orderId);
+      const order = await ctx.service.order.findByID(orderToken.orderId);
+      const user = await ctx.service.users.getUserProfile(orderToken.userId);
+      await ctx.service.orderToken.setPicked(orderToken.orderId);
       ctx.body = {
         code: 0,
         msg: '已取餐',
