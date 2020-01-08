26addf11b9406fe59353cf48da2cdccc06290898
Tinko <TinkoLiu@users.noreply.github.com>
2020-01-08 16:06:51 +0800

    config: update mongodb driver config

--- config/config.default.js
+++ config/config.default.js
@@ -56,6 +56,7 @@ module.exports = appInfo => {
       options: {
         useCreateIndex: true,
         useNewUrlParser: true,
+        useFindAndModify: false,
         useUnifiedTopology: true
       }
     }
