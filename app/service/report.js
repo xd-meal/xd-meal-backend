db2a07219708ab8c10037382d5e7bc06558ff8ed
Tinko <TinkoLiu@users.noreply.github.com>
2020-01-10 17:04:17 +0800

    feat: add personal detail

--- app/service/report.js
+++ app/service/report.js
@@ -99,5 +99,60 @@ class ReportService extends Service {
       }
     ])
   }
+
+  async getPersonalReport (dinings, corp) {
+    const ctx = this.ctx
+    const OrderModel = ctx.model.Order
+    return OrderModel.aggregate([
+      {
+        $match: {
+          dining_id: {
+            $in: dinings.map(el => ObjectId(el))
+          }
+        }
+      }, {
+        $lookup: {
+          from: 'user',
+          localField: 'uid',
+          foreignField: '_id',
+          as: 'userInfo'
+        }
+      }, {
+        $unwind: {
+          path: '$userInfo'
+        }
+      }, {
+        $addFields: {
+          corp: '$userInfo.wechat_corpid'
+        }
+      }, {
+        $match: {
+          corp
+        }
+      }, {
+        $lookup: {
+          from: 'dish',
+          localField: 'menu_id',
+          foreignField: '_id',
+          as: 'dishInfo'
+        }
+      }, {
+        $unwind: {
+          path: '$dishInfo'
+        }
+      }, {
+        $lookup: {
+          from: 'dining',
+          localField: 'dining_id',
+          foreignField: '_id',
+          as: 'diningInfo'
+        }
+      }, {
+        $unwind: {
+          path: '$diningInfo'
+        }
+      }
+    ])
+  }
 }
 module.exports = ReportService
