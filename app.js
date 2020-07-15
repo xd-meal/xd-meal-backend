19eac4d6c10135cb40e0fc146dbe82c8832507f7
Tinko <TinkoLiu@users.noreply.github.com>
2020-07-15 15:40:25 +0800

    feat: set button for wework on boot

--- app.js
+++ app.js
@@ -18,5 +18,36 @@ class AppBootHook {
       await this.app.redis.set('dining_roll_' + el._id, el.order_end.getTime())
     }
   }
+
+  async didReady () {
+    const config = this.ctx.app.config
+    const corps = Object.keys(config?.wework?.corpID || {})
+    for (let index = 0; index < corps.length; index++) {
+      const corp = corps[index]
+      const accessToken = await this.ctx.service.wework.getAccessToken(corp)
+      await this.ctx.curl(
+        'https://qyapi.weixin.qq.com/cgi-bin/menu/create?access_token=' +
+        accessToken +
+        '&agentid=' +
+        config.wework.agentID[corp],
+        {
+          method: 'POST',
+          contentType: 'json',
+          data: {
+            button: [
+              {
+                type: 'view',
+                name: '吃啥',
+                key: 'https://order.xindong.com/?wework_source=' + corp,
+                sub_button: [],
+                url: 'https://order.xindong.com/?wework_source=' + corp
+              }
+            ]
+          },
+          dataType: 'json'
+        }
+      )
+    }
+  }
 }
 module.exports = AppBootHook
