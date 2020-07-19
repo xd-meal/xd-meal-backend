7fa3930995c779c8306758f372a0395b929a812f
Tinko <TinkoLiu@users.noreply.github.com>
2020-07-19 20:41:11 +0800

    feat: add pick order by user

--- app/controller/pos.js
+++ app/controller/pos.js
@@ -69,5 +69,76 @@ class PosController extends Controller {
       }
     }
   }
+
+  async pickByNfcUid () {
+    const ctx = this.ctx
+    const diningService = ctx.service.dining
+    const orderService = ctx.service.order
+    const userService = ctx.service.users
+    const user = await userService.getUserByNfcUid(ctx.params.nfcuid)
+    if (!user) {
+      throw new HttpError({
+        code: 403,
+        msg: '无效的 NFC 标签'
+      })
+    }
+    delete user.password
+    delete user.psw_salt
+    const pickableDinings = await diningService.getAllPickableDinings()
+    const nonOrders = []
+    const pickableIDs = pickableDinings.reduce((acc, cur) => {
+      acc.push(cur._id)
+      if (cur.stat_type === 0) {
+        nonOrders.push(cur)
+      }
+      return acc
+    }, [])
+    const myDish = await orderService.findPickableOrderByUserAndDiningIDs(ctx.session.user._id, pickableIDs)
+    if (!myDish.length && !nonOrders.length) {
+      throw new HttpError({
+        code: 403,
+        msg: '目前没有可取餐次'
+      })
+    } else if (myDish.length) {
+      const currentOrder = myDish[0]
+      const currentDining = pickableDinings.find(el => {
+        return el._id.toString() === currentOrder.dining_id.toString()
+      })
+      const user = await ctx.service.users.getUserProfile(currentOrder.uid)
+      const dishIndex = currentDining.menu.findIndex(el => {
+        return el._id.toString() === currentOrder.menu_id.toString()
+      })
+      await ctx.service.order.setPicked(currentOrder._id)
+      ctx.body = {
+        code: 0,
+        msg: '已取餐',
+        data: {
+          currentDining,
+          currentOrder,
+          dishIndex,
+          dish: dining.menu[dishIndex],
+          user
+        }
+      }
+    } else if (nonOrders.length) {
+      const currentDining = nonOrders[0]
+      const order = await orderService.getByUserAndDiningID(user._id, currentDining._id)
+      if (order) {
+        throw new HttpError({
+          code: 404,
+          msg: '目前没有可取餐次'
+        })
+      }
+      await ctx.service.order.setPicked(currentOrder._id)
+      ctx.body = {
+        code: 0,
+        msg: '已取餐',
+        data: {
+          currentDining,
+          user
+        }
+      }
+    }
+  }
 }
 module.exports = PosController
